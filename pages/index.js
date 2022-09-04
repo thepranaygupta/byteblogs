bfc99285947ef886361b4f472b9e8fc9ad348961
Pranay <pranayguptamain@gmail.com>
2022-09-04 13:20:36 +0530

    homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,33 @@
 import Head from 'next/head'
+import BlogCard from '../components/BlogCard'
 
-export default function Home() {
+export default function Home({ data }) {
+  // console.log(data)
+
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>ByteBlogs</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="This is the home page" />
       </Head>
-      <h1 className="text-3xl font-bold underline text-center">Home</h1>
+      <div>
+        {data.data.map((blog) => (
+          <BlogCard blog={blog} key={blog.id} />
+        ))}
+      </div>
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://byteblogs.herokuapp.com/api/blogs?populate=*')
+  const data = await res?.json()
+
+  return {
+    props: {
+      data
+    },
+    revalidate: 1
+  }
 }
